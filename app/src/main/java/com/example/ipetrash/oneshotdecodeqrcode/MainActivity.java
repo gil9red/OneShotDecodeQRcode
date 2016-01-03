1e448b9208a1a0fc265aeae2704716fe2b183d95
Ilya Petrash <ilya.petrash@inbox.ru>
2016-01-03 18:57:47 +0500

    refactoring

--- app/src/main/java/com/example/ipetrash/oneshotdecodeqrcode/MainActivity.java
+++ app/src/main/java/com/example/ipetrash/oneshotdecodeqrcode/MainActivity.java
@@ -1,17 +1,11 @@
 package com.example.ipetrash.oneshotdecodeqrcode;
 
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.graphics.Bitmap;
-import android.hardware.Camera;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -22,26 +16,46 @@ import com.google.zxing.BinaryBitmap;
 import com.google.zxing.MultiFormatReader;
 import com.google.zxing.NotFoundException;
 import com.google.zxing.RGBLuminanceSource;
-import com.google.zxing.Result;
 import com.google.zxing.common.HybridBinarizer;
 
 
 public class MainActivity extends ActionBarActivity {
-
-    public Camera camera;
-    public SurfaceHolder surfaceHolder;
-
-    public boolean cameraOn = false;
-
     private static final int CAMERA_REQUEST = 1888;
     private ImageView imageView;
+    private TextView textView;
+    private String TAG = "com.ipetrash";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.imageView = (ImageView) this.findViewById(R.id.imageView);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        textView = (TextView) findViewById(R.id.textView);
+
+        Button buttonPhoto = (Button) findViewById(R.id.buttonPhoto);
+        buttonPhoto.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.d(TAG, "Photo click");
+
+                        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                    }
+                }
+        );
+
+        Button buttonQuit = (Button) findViewById(R.id.buttonQuit);
+        buttonQuit.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.d(TAG, "Quit click");
+                        finish();
+                    }
+                }
+        );
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -50,53 +64,42 @@ public class MainActivity extends ActionBarActivity {
             imageView.setImageBitmap(photo);
 
             String text = decode(photo);
+            Log.d(TAG, text != null ? String.format("decode text = '%s'", text) : "text is null");
 
-            TextView textView = (TextView) findViewById(R.id.textView);
-            textView.setText(text != null ? text : "ERROR: Не получилось разобрать QR code");
-
-//            photo.recycle();
-//            photo = null;
+            text = text != null ? text : "ERROR: Не получилось разобрать QR code";
+            textView.setText(text);
         }
-    }
-
-    public void doDecode(View view) {
-        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(cameraIntent, CAMERA_REQUEST);
     }
 
     public String decode(Bitmap image) {
+        Log.d(TAG, "decode start");
+
         int width = image.getWidth(), height = image.getHeight();
         int[] pixels = new int[width * height];
         image.getPixels(pixels, 0, width, 0, 0, width, height);
-
-        Log.d("qrcode", "pixels=" + pixels);
+        Log.d(TAG, "pixels=" + pixels);
 
         RGBLuminanceSource source = new RGBLuminanceSource(width, height, pixels);
-
-        Log.d("qrcode", "source=" + source);
+        Log.d(TAG, "source=" + source);
 
         BinaryBitmap bBitmap = new BinaryBitmap(new HybridBinarizer(source));
-
-        Log.d("qrcode", "bBitmap=" + bBitmap);
+        Log.d(TAG, "bBitmap=" + bBitmap);
 
         MultiFormatReader reader = new MultiFormatReader();
+        Log.d(TAG, "reader=" + reader);
 
-        Log.d("qrcode", "reader=" + reader);
+        try {
+            String result = reader.decode(bBitmap).getText();
+            Log.d(TAG, "result=" + result);
 
-        try
-        {
-            Result result = reader.decode(bBitmap);
-            Log.d("qrcode", "result=" + result);
-            return result.getText();
-        }
-        catch (NotFoundException e)
-        {
-            Log.e("qrcode", "decode exception", e);
+            return result;
+
+        } catch (NotFoundException e) {
+            Log.e(TAG, "decode exception", e);
             return null;
         }
-    }
-
-    public void quit(View view) {
-        finish();
+        finally {
+            Log.d(TAG, "decode end");
+        }
     }
 }
